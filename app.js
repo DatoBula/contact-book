f05464aeef8f2bccc6a085d052bb729c702b9062
დავით აბულაშვილი <datobula@mia.gov.ge>
2016-04-07 16:16:51 +0400

    ref

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var MongoClient = require('mongodb').MongoClient
@@ -7,34 +8,37 @@ var url = 'mongodb://localhost:27017/site';
 // Initialize connection once
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/list', function (req, res) {
-    console.log('request')
-    db.collection('documents', function (err, collection) {
-        collection.find({}).toArray(function (err, docs) {
-            console.log(err)
-            console.log("Found the following records");
-            console.log(docs);
-            res.send(JSON.stringify(docs));
-        });
+app.get('/list', function(req, res) {
+  console.log('request')
+  db.collection('documents', function(err, collection) {
+    collection.find({}).toArray(function(err, docs) {
+      console.log(err)
+      console.log("Found the following records");
+      console.log(docs);
+      res.send(JSON.stringify(docs));
     });
+  });
 });
 
-app.get('/skills', function (req, res) {
-    res.send(JSON.stringify(['თოხვა', 'ბარვა', 'თიბვა']));
+app.get('/skills', function(req, res) {
+  res.send(JSON.stringify(['თოხვა', 'ბარვა', 'თიბვა']));
 });
 
-app.post('/add', function (req, res) {
-    // TODO insert document
+app.post('/add', function(req, res) {
+  console.log(rep.body);
 });
 
-MongoClient.connect(url, function (err, database) {
-    if (err) throw err;
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
 
-    db = database;
+  db = database;
 
-    // Start the application after the database connection is ready
-    app.listen(80, function () {
-        console.log('Example app listening on port 80!');
-    });
+  // Start the application after the database connection is ready
+  app.listen(3030, function() {
+    console.log('Example app listening on port 3030!');
+  });
 });
--- public/js/site.js
+++ public/js/site.js
@@ -1,37 +1,86 @@
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-    //$('select').material_select();
+$(document).ready(function() {
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal({
+    dismissible: false
+  });
 
-    setTimeout(function(){
-        $('select').material_select();
-    },1000);
+  $('.datepicker').pickadate({
+    selectYears: 100,
+    selectMonths: true,
+    monthsFull: ['იანვარი', 'თებერვალი', 'მარტი', 'აპრილი', 'მაისი', 'ივნისი', 'ივლისი', 'აგვისტო', 'სექტემბერი', 'ოქტომბერი', 'ნოემბერი', 'დეკემბერი'],
+    monthsShort: ['იან', 'თებ', 'მარ', 'აპრ', 'მაი', 'ივნ', 'ივლ', 'აგვ', 'სექ', 'ოქტ', 'ნოე', 'დეკ'],
+    weekdaysFull: ['კვირა', 'ორშაბათი', 'სამშაბათი', 'ოთხშაბათი', 'ხუთშაბათი', 'პარასკევი', 'შაბათი'],
+    weekdaysShort: ['კვ', 'ორ', 'სამ', 'ოთხ', 'ხუთ', 'პარ', 'შაბ'],
+    weekdaysLetter: ['კ', 'ო', 'ს', 'ო', 'ხ', 'პ', 'შ'],
+    firstDay: 1
+  });
 
-    $('.datepicker').pickadate({
-        selectYears: 100,
-        selectMonths: true,
-        monthsFull: ['იანვარი', 'თებერვალი', 'მარტი', 'აპრილი', 'მაისი', 'ივნისი', 'ივლისი', 'აგვისტო', 'სექტემბერი', 'ოქტომბერი', 'ნოემბერი', 'დეკემბერი'],
-        monthsShort: ['იან', 'თებ', 'მარ', 'აპრ', 'მაი', 'ივნ', 'ივლ', 'აგვ', 'სექ', 'ოქტ', 'ნოე', 'დეკ'],
-        weekdaysFull: ['კვირა', 'ორშაბათი', 'სამშაბათი', 'ოთხშაბათი', 'ხუთშაბათი', 'პარასკევი', 'შაბათი'],
-        weekdaysShort: ['კვ', 'ორ', 'სამ', 'ოთხ', 'ხუთ', 'პარ', 'შაბ'],
-        weekdaysLetter: [ 'კ', 'ო', 'ს', 'ო', 'ხ', 'პ', 'შ' ],
-        firstDay: 1
-    });
+  $.get("skills", function(data) {
+    var options = JSON.parse(data)
+    var skills = $('#skills');
+    for (var i in options) {
+      var option = document.createElement("option");
+      option.innerHTML = options[i]
+      skills.append(option)
+    }
+    $('#skills').material_select();
+  });
 });
 
 var app = angular.module("site", []);
-app.controller('controller', function ($scope, $http) {
-    $http.get("list").then(function (response) {
-        $scope.persons = response.data;
-        setTimeout(function(){
-            $('.collapsible').collapsible({});
-        },500);
-    });
-
-    $http.get("skills").then(function (response) {
-        $scope.skills = response.data;
-        setTimeout(function(){
-            $('select').material_select();
-        },500);
-    });
+app.controller('controller', function($scope, $http) {
+  $http.get("list").then(function(response) {
+    $scope.persons = response.data;
+    setTimeout(function() {
+      $('.collapsible').collapsible({});
+    }, 500);
+  });
 });
+
+app.directive('fileModel', ['$parse', function($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.fileModel);
+      var modelSetter = model.assign;
+
+      element.bind('change', function() {
+        scope.$apply(function() {
+          modelSetter(scope, element[0].files[0]);
+        });
+      });
+    }
+  };
+}]);
+
+app.controller('addForm', ['$scope', function($scope) {
+  $scope.submit = function() {
+    var user = $scope.user;
+    console.dir(user);
+    var fd = new FormData();
+    angular.forEach(user, function(value, key) {
+      fd.append(key, value);
+    });
+    $.ajax({
+      url: 'add',
+      data: fd,
+      cache: false,
+      contentType: false,
+      processData: false,
+      type: 'POST',
+      success: function(response) {
+        deferred.resolve(response);
+        $rootScope.requestInProgress = false;
+      },
+      error: function(error) {
+        deferred.reject(error);
+        $rootScope.requestInProgress = false;
+      }
+    });
+  };
+
+  $scope.cancel = function() {
+    console.log("cancel");
+    $scope.user = {}
+  };
+}]);
